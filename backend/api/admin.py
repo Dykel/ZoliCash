857d39693795680fb21bdc9aa443776d5e1bf761
Joseph Jonathan Collet <dykelaway@gmail.com>
2025-02-23 18:13:24 +0400

    Register Company, Sale, Task models in Django admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Company, Sale, Task
 
-# Register your models here.
+admin.site.register(Company)
+admin.site.register(Sale)
+admin.site.register(Task)
